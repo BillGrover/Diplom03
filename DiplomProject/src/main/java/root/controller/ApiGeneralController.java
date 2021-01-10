09e2acfaa85327d0825edae5b3533817a8f4b1cc
BillGrover <belllev@yandex.ru>
2021-01-10 12:30:24 +0500

    006 api/init done

--- DiplomProject/src/main/java/root/controller/ApiGeneralController.java
+++ DiplomProject/src/main/java/root/controller/ApiGeneralController.java
@@ -1,9 +1,23 @@
 package root.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
 public class ApiGeneralController {
+
+    @GetMapping("/init")
+    @ResponseBody
+    public String init(){
+        return
+        "{\"title\": \"DevPub\"," +
+                "\"subtitle\": \"Рассказы разработчиков\"," +
+                "\"phone\": \"+7 903 666-44-55\"," +
+                "\"email\": \"mail@mail.ru\"," +
+                "\"copyright\": \"Дмитрий Сергеев\"," +
+                "\"copyrightFrom\": \"2005\"}";
+    }
 }
